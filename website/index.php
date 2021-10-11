d71587607a599f8e7d6ae1803562a7930cccf85b
LiamOsler <liam.osler@dal.ca>
2021-10-11 00:57:31 -0300

    experimentation with displaying the articles

--- website/index.php
+++ website/index.php
@@ -6,25 +6,57 @@
 
 <div class = "container">
     <div class = "row">
+
     <h1>Latest News</h1>
 
 
         <?php
-            $querySQL = "SELECT * FROM articles";
+            $querySQL = "   SELECT * FROM articles
+                            JOIN outlets ON articles.outletID = outlets.outletID
+                            LEFT JOIN (
+                            SELECT articleID, COUNT(DISTINCT(reactionID)) AS articleReactionCount
+                            FROM articlereactions
+                            GROUP BY articleID) articlereactions on articles.articleID = articlereactions.articleID
+                            LEFT JOIN (
+                            SELECT articleID, COUNT(DISTINCT(commentID)) AS articleCommentCount
+                            FROM articlecomments
+                            GROUP BY articleID) articlecomments on articles.articleID = articlecomments.articleID";
             $result = $dbconn->query($querySQL);
 
             foreach($result as $article){
             ?>
-            <div class="col-sm-16 col-md-4 col-lg-3">
-            <div class="card bg-light mb-3">
-                <div class="card-header"><?php echo($article["authorName"])?></div>
-                    <div class="card-body">
+            <div class="col-md-6">
+                <div class="card bg-light mb-3">
+                    <div class="card-header"> 
                         <a href = "<?php echo($article["articleURL"])?>">
-                            <h5 class="card-title"><?php echo($article["articleText"])?></h5>
-                        </a>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-            </div>
+                            <h5 class="card-title"><?php echo($article["articleHeadline"])?></h5>
+                        </a><?php echo($article["authorName"])?></div>
+                        <div class="card-body">
+                        
+                            <p class="card-text"><?php echo($article["articleText"])?></p>
+
+                            <p>
+                                <?php 
+                                    if($article["articleReactionCount"]){
+                                        echo($article["articleReactionCount"] . " Reactions");
+                                    }
+                                    else{
+                                        echo("No Reactions");
+                                    }
+                                ?>
+                                and
+                                <?php 
+                                    if($article["articleCommentCount"]){
+                                        echo($article["articleCommentCount"] . " Comments");
+                                    }
+                                    else{
+                                        echo("No comments");
+                                    }
+                                ?>
+                            </p>
+
+                        </div>
+                </div>
             </div>
             
 
@@ -36,5 +68,8 @@
     </div>
 </div>
 </main>
+
+<script>
+</script>
 
 <?php include "inc/footer.php" ?>

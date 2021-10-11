74c7b9dd13a2d0e53c272975ed11d5a1cdc6f5b7
LiamOsler <liam.osler@dal.ca>
2021-10-10 23:03:14 -0300

    Added bootstrap cards and basic latest news queries to homepage

--- website/inc/header.php
+++ website/inc/header.php
@@ -59,12 +59,3 @@
   </div>
 </div>
 
-<form>
-    <div class="form-group">
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login <i class="bi-person-circle" style="font-size: 1rem;"></i></button>
-</form>
--- website/index.php
+++ website/index.php
@@ -5,23 +5,36 @@
 <main>
 
 <div class = "container">
-<h1>Latest News</h1>
+    <div class = "row">
+    <h1>Latest News</h1>
 
-<pre>
-<?php
-    $querySQL = "SELECT * FROM articles";
-    $result = $dbconn->query($querySQL);
 
-    print_r($result);
+        <?php
+            $querySQL = "SELECT * FROM articles";
+            $result = $dbconn->query($querySQL);
 
-    foreach($result as $article){
-        print_r($article);
-    }
-?>
-</pre>
+            foreach($result as $article){
+            ?>
+            <div class="col-sm-16 col-md-4 col-lg-3">
+            <div class="card bg-light mb-3">
+                <div class="card-header"><?php echo($article["authorName"])?></div>
+                    <div class="card-body">
+                        <a href = "<?php echo($article["articleURL"])?>">
+                            <h5 class="card-title"><?php echo($article["articleText"])?></h5>
+                        </a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+            </div>
+            </div>
+            
 
+        <?php
+            } //Close foreach($result as $article)
+        ?>
+
+
+    </div>
 </div>
-
 </main>
 
 <?php include "inc/footer.php" ?>

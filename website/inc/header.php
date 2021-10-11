db9a69979830a34443355a37e5baa242d377c2e4
LiamOsler <liam.osler@dal.ca>
2021-10-11 00:57:53 -0300

    changes to the navbar

--- website/inc/header.php
+++ website/inc/header.php
@@ -13,33 +13,44 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
-
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <link href="styles/main.css" rel="stylesheet">
+    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 
     <title>Nova Scotia Courts Website</title>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Nova Scotia Legal News</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Latest News<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
         <a class="nav-item nav-link" href="#">Rulings</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-item nav-link" href="#">Legislation</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-item nav-link" href="#">News & Opinion</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-item nav-link" href="#">About</a>
-      
-    </div>
+      </li>
+    </ul>
+    <span class="navbar-text">
+        <a href="#" style = "text-decoration: none">Login &nbsp;<i class="bi-person-circle" style="font-size: 1em"></i></i></a>    
+    </span>
   </div>
-
-
 </nav>
+
 
 
 <div class="jumbotron jumbotron-fluid">

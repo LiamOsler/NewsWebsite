af30280020ab62f13a4a7c41a2da0489d4240a03
LiamOsler <liam.osler@dal.ca>
2021-10-12 00:07:41 -0300

    update database scripts

--- database_management/useful_queries.sql
+++ database_management/useful_queries.sql
@@ -1,10 +1,15 @@
--- Get the contents of the articles and their reaction and comment counts:
+-- Get the contents of the articles and their reaction and comment counts
 SELECT * FROM articles
 JOIN outlets ON articles.outletID = outlets.outletID
 LEFT JOIN (
-SELECT COUNT(DISTINCT(reactionID)) AS articleReactionCount
+SELECT articleID, COUNT(DISTINCT(reactionID)) AS articleReactionCount
 FROM articlereactions
-GROUP BY articleID) articlereactions on articles.articleID = articlereactions.articleID;
+GROUP BY articleID) articlereactions on articles.articleID = articlereactions.articleID
+LEFT JOIN (
+SELECT articleID, COUNT(DISTINCT(commentID)) AS articleCommentCount
+FROM articlecomments
+GROUP BY articleID) articlecomments on articles.articleID = articlecomments.articleID
+ORDER BY articles.articleID DESC;
 
 
 

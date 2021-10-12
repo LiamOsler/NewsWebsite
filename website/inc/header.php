2b5b90e185e8098417cc68533574e891fc363c2f
LiamOsler <liam.osler@dal.ca>
2021-10-12 04:02:58 -0300

    add navbar link to index

--- website/inc/header.php
+++ website/inc/header.php
@@ -24,7 +24,7 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Nova Scotia Legal News</a>
+  <a class="navbar-brand" href="index.php">Nova Scotia Legal News</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>

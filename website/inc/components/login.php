42b4cf8424954f86f5d28ce86af184266c0b7f10
LiamOsler <liam.osler@dal.ca>
2021-10-12 04:03:15 -0300

    adjust login modal size

--- website/inc/components/login.php
+++ website/inc/components/login.php
@@ -32,7 +32,7 @@
 
 <!-- Account Creation Modal-->
 <div class="modal fade" id="newAccountModal" tabindex="-1" role="dialog" aria-labelledby="newAccountModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="newAccountModalLabel">Create an Account</h5>

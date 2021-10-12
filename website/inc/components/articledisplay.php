8449d00fb57556c74927fbef46670403eb7240b0
LiamOsler <liam.osler@dal.ca>
2021-10-12 04:27:53 -0300

    edit article display to use new query

--- website/inc/components/articledisplay.php
+++ website/inc/components/articledisplay.php
@@ -1,3 +1,17 @@
 <?php
+$articleID = $_GET["articleID"];
 
+$querySQL = "   SELECT * FROM articles
+                JOIN outlets ON articles.outletID = outlets.outletID 
+                AND articles.articleID = {$articleID}";
+
+$result = $dbconn->query($querySQL);
+foreach($result as $article){
+?>
+<h1><?php echo($article["articleHeadline"]); ?></h1>
+<p><?php echo($article["outletName"]); ?></p>
+<p><?php echo($article["articleText"]); ?></p>
+
+<?php
+}
 ?>

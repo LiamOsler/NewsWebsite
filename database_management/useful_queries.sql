0f19874d4d1b50a3ea33d5bf128774075913cf03
LiamOsler <liam.osler@dal.ca>
2021-10-12 04:27:41 -0300

    fix articleID issue- removed use of *

--- database_management/useful_queries.sql
+++ database_management/useful_queries.sql
@@ -1,5 +1,5 @@
 -- Get the contents of the articles and their reaction and comment counts, order with newest posts first
-SELECT * FROM articles
+SELECT articles.articleID, articles.articleText articles.articleCommentCount FROM articles
 JOIN outlets ON articles.outletID = outlets.outletID
 LEFT JOIN (
 SELECT articleID, COUNT(DISTINCT(reactionID)) AS articleReactionCount
@@ -33,18 +33,18 @@ AND articles.articleID = 1;
 
 -- Get the count of total reactions for the article:
 SELECT articleID, COUNT(reactionID) AS articleReactionCount
-FROM articlereactions
+FROM articleReactions
 WHERE articleID = 1;
 
 -- Get the count of likes for the post:
 SELECT articleID, COUNT(reactionID) AS "LIKES"
-FROM articlereactions
+FROM articleReactions
 WHERE reactionType = "LIKE"
 AND articleID = 1;
 
 -- Get the count of likes for the post:
 SELECT articleID, COUNT(reactionID) AS "DISLIKES"
-FROM articlereactions
+FROM articleReactions
 WHERE reactionType = "DISLIKE"
 AND articleID = 1;
 
@@ -52,17 +52,15 @@ AND articleID = 1;
 SELECT articleID,
 COUNT(CASE WHEN reactionType = "LIKE" THEN 1 END)  AS "LIKES",
 COUNT(CASE WHEN reactionType = "DISLIKE" THEN 1 END) AS "DISLIKES"
-FROM articlereactions
+FROM articleReactions
 WHERE articleID = 1;
 
+SELECT * FROM articles
+WHERE articleID = 1;
 
-
--- Get the comments, and usernames for a specific post:
-SELECT commentID, commentText, userName FROM articleComments
-JOIN articles ON articleComments.articleID = articles.articleID
-JOIN users ON articleComments.userID = users.userID;
-
-
+SELECT * FROM articles
+JOIN outlets ON articles.outletID = outlets.outletID 
+AND articles.articleID = 1;
 
 
 

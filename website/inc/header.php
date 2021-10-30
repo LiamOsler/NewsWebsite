aafe7898888becea81c4c877898b909464a0455c
LiamOsler <liam.osler@dal.ca>
2021-10-29 23:02:43 -0300

    move search jumbotron to new file

--- website/inc/header.php
+++ website/inc/header.php
@@ -56,24 +56,6 @@
 </nav>
 
 
-
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Nova Scotia Legal News</h1>
-    <p class="lead">A place to find and comment on the latest rulings, legislation and other legal affairs in Nova Scotia</p>
-    <form>
-        <div class="input-group input-group-lg">
-            <input type="text" class="form-control" placeholder="Search">
-                <div class="input-group-btn">
-                    <button  class="btn btn-info btn-lg" type="submit"><i class="bi-search" style="font-size: 1.5rem;"></i></button>
-                </div>
-            </div>
-    </form>
-
-    <br>
-  </div>
-</div>
-
 <?php include "inc/components/login.php" ?>
 
 

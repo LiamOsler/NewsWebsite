c6854cd8ec7466eb9bfdeac0cb6a788b52e430c7
Rachel Woodside <rachel.woodside3@gmail.com>
2021-12-07 23:10:02 -0400

    added contact form with postback processing

--- website/about.php
+++ website/about.php
@@ -1,5 +1,25 @@
 <!--Edited by Adam Melvin - B00597004-->
-<?php include "inc/header.php";
+<?php 
+    include "inc/header.php";
+
+    $contactFormFeedback = "";
+
+    // Postback processing for contact form
+    if (isset($_POST["contact-us"])) {
+        // Check if the inputs are empty and display error messages
+        if (empty($_POST["contact-name"])) {
+            $contactFormFeedback = "<p>Please provide your name so we can return your message.</p>";
+        }
+        else if (empty($_POST["contact-msg"])) {
+            $contactFormFeedback = "<p>Please provide a message.</p>";
+        }
+        else {
+            $contactName = sanitizeData($_POST["contact-name"]);
+            $contactMsg = sanitizeData($_POST["contact-msg"]);
+            $contactFormFeedback = "<p>Message sent. Thank you for contacting us at NSLN, " . $contactName . "!</p>";
+        }
+    }
+
 ?>
 
 <main>
@@ -11,7 +31,6 @@
                 and legal professionals alike.
             </p>
         </div>
-
     </div>
     <div class="container">
         <p class="lead">
@@ -27,5 +46,22 @@
             Have a particular area of law that you're interested in? On NSLN, you can stay up to date and
             "follow" specific topics, rulings, or journalists you are interested in.
         </p>
+    </div>
+    <div class="container">
+        <h2>Contact Us</h2>
+        <div id="contact-form-feedback">
+            <?php echo $contactFormFeedback;?>
+        </div>
+        <form action="about.php" method="POST">
+            <div class="form-group">
+                <label for="contact-name">Enter your name:</label>
+                <input type="text" class="form-control" id="contact-name" placeholder="Name" name="contact-name" >
+            </div>
+            <div class="form-group">
+                <label for="contact-msg">Message:</label>
+                <textarea class="form-control" id="contact-msg" placeholder="Enter your message here..." name="contact-msg" rows=5></textarea>
+            </div>
+            <button class="btn btn-success btn-lg" name="contact-us" type="submit" >Submit Message</button>
+        </form>
     </div>
 </main>
